aaabb056e58a348831242a7a0dda49fd132f999c
Luis Rodriguez <luis.rodriguez@cru.org>
2014-08-02 00:02:42 -0400

    fixing variables

--- splunk_cru_config/recipes/default.rb
+++ splunk_cru_config/recipes/default.rb
@@ -1,21 +1,21 @@
-
 # Cookbook Name:: splunk_cru_config
 # Recipe:: default
-#
 
-#
-
-#
 #configure splunk
+fw_server = #node{['splunk']['forward_server']}
+auth_user = #node{['splunk']['auth']}
+default_pass = #node{['splunk']['pass']}
+new_pass = #node{['splunk']['newpass']}
+
 bash 'splunk_conf' do
   if !File.exists?("#{node['splunk']['install_path']}/etc/apps/search/local/inputs.conf")
     code <<-EOH
     cd "#{node['splunk']['install_path']}/bin"
     ./splunk start --accept-license
     ./splunk enable boot-start
-    ./splunk add forward-server #{node['splunk']['forward_server']}:#{node['splunk']['receiver_port']} -auth #{node['splunk']['auth']}:#{node['splunk']['pass']}
+    ./splunk add forward-server #{fw_server}:9997 -auth #{auth_user}:#{default_pass}
     ./splunk add monitor #{node['splunk']['monitor_path']}
-    ./splunk edit user #{node['splunk']['auth']} -password #{node['splunk']['newpass']} -auth #{node['splunk']['auth']}:#{node['splunk']['pass']}
+    ./splunk edit user #{auth_user} -password #{new_pass} -auth #{auth_user}:#{new_pass}
     ./splunk restart
     EOH
   end
