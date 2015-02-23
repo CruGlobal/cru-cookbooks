9f6081d3a45f32f81e227c96c7ee62e0840f763a
Josh Starcher <josh.starcher@gmail.com>
2015-02-23 08:11:13 -0800

    Upgrade bundler before doing bundle config

--- bundler/recipes/setup.rb
+++ bundler/recipes/setup.rb
@@ -1,4 +1,9 @@
 if node[:bundle_config]
+  execute "upgrade bundler" do
+    command "/usr/bin/sudo gem install bundler"
+    action :run
+  end
+
   node[:bundle_config].each do |config|
     execute "configure bundler" do
       command "/usr/bin/sudo su deploy -c 'bundle config #{config}'"
