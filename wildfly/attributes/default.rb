b8205becda51847ca978c5af28f4cbe9e0be5f08
ryancarlson <ryan.t.carlson@cru.org>
2015-04-29 15:31:14 -0400

    update max_permgen_space default to 256m

--- wildfly/attributes/default.rb
+++ wildfly/attributes/default.rb
@@ -1,4 +1,4 @@
 default['wildfly']['initial_heap_size'] = '64m'
 default['wildfly']['max_heap_size'] = '512m'
-default['wildfly']['max_permgen_space'] = '512m'
+default['wildfly']['max_permgen_space'] = '256m'
 default['wildfly']['console_log_level'] = 'INFO'
